ef629564542f8454483bdb81eeb948ab753ebbe8
k-scibor <uT_3780@zsp3zamosc.pl>
2025-01-23 09:01:36 +0100

    Delete bledyLogiczne4 directory

--- bledyLogiczne4/bledyLogiczne4/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-/*Program powienien obliczać średnią ilość paliwa zużytego na przejechanie 100km*/
-Console.WriteLine("Podaj ilość przejechanyh kilometrów: ");
-double km = double.Parse(Console.ReadLine());
-Console.WriteLine("Podaj ilość spalonego paliwa: ");
-double p = double.Parse(Console.ReadLine());
-double pNa100km = km / 100 / p; 
-Console.WriteLine("Spalanie wyniosło średnio " +  pNa100km + "l na 100km");
